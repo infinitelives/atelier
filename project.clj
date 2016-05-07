f1c89e52ef45c6627bdd9dc2880830951fb383bf
Crispin Wellington <retrogradeorbit@gmail.com>
2016-05-08 01:17:30 +0800

    devcards project file

--- project.clj
+++ project.clj
@@ -5,12 +5,17 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :min-lein-version "2.6.1"
-  
+
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.8.51"]
                  [org.clojure/core.async "0.2.374"
-                  :exclusions [org.clojure/tools.reader]]]
-  
+                  :exclusions [org.clojure/tools.reader]]
+
+                 [devcards "0.2.1-6"]
+                 [reagent "0.5.1"]
+                 [cljsjs/highlight "8.4-0"]
+                 ]
+
   :plugins [[lein-figwheel "0.5.3"]
             [lein-cljsbuild "1.1.3" :exclusions [[org.clojure/clojure]]]]
 
@@ -19,7 +24,17 @@
   :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
 
   :cljsbuild {:builds
-              [{:id "dev"
+              [
+               {:id "devcards"
+                :source-paths ["src"]
+                :figwheel { :devcards true }
+                :compiler { :main       "atelier.core"
+                           :asset-path "js/compiled/devcards_out"
+                           :output-to  "resources/public/js/compiled/atelier_devcards.js"
+                           :output-dir "resources/public/js/compiled/devcards_out"
+                           :source-map-timestamp true }    }
+
+               {:id "dev"
                 :source-paths ["src"]
 
                 ;; If no code is to be run, set :figwheel true for continued automagical reloading
@@ -40,7 +55,7 @@
                            :optimizations :advanced
                            :pretty-print false}}]}
 
-  :figwheel {;; :http-server-root "public" ;; default and assumes "resources"
+  :figwheel { ;; :http-server-root "public" ;; default and assumes "resources"
              ;; :server-port 3449 ;; default
              ;; :server-ip "127.0.0.1"
 
@@ -53,7 +68,7 @@
              ;; if you want to embed a ring handler into the figwheel http-kit
              ;; server, this is for simple ring servers, if this
              ;; doesn't work for you just run your own server :) (see lien-ring)
-             
+
              ;; :ring-handler hello_world.server/handler
 
              ;; To be able to open files in your editor from the heads up display
@@ -64,7 +79,7 @@
              ;; emacsclient -n +$2 $1
              ;;
              ;; :open-file-command "myfile-opener"
-             
+
              ;; if you are using emacsclient you can just use
              ;; :open-file-command "emacsclient"
 
@@ -75,17 +90,17 @@
              ;; :server-logfile "tmp/logs/figwheel-logfile.log"
              }
 
- 
+
   ;; setting up nREPL for Figwheel and ClojureScript dev
   ;; Please see:
   ;; https://github.com/bhauman/lein-figwheel/wiki/Using-the-Figwheel-REPL-within-NRepl
-  
+
   :profiles {:dev {:dependencies [[figwheel-sidecar "0.5.3"]
                                   [com.cemerick/piggieback "0.2.1"]]
                    ;; for CIDER
                    ;; :plugins [[cider/cider-nrepl "0.12.0"]]
-                   :repl-options {; for nREPL dev you really need to limit output
+                   :repl-options { ; for nREPL dev you really need to limit output
                                   :init (set! *print-length* 50)
                                   :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
 
-)
+  )
