db919f5ae836e7a2d0ca129986e2b55bec408dc7
Crispin Wellington <retrogradeorbit@gmail.com>
2016-05-12 01:27:54 +0800

    no core devcards

--- src/atelier/core.cljs
+++ src/atelier/core.cljs
@@ -1,7 +1,9 @@
 (ns atelier.core
   (:require [reagent.core :as reagent]
+            [atelier.canvas :as canvas]
             [atelier.code :as code]
-            [atelier.canvas :as canvas])
+
+            )
   (:require-macros
    [devcards.core :as dc :refer [defcard deftest defcard-rg defcard-doc]])
   )
@@ -24,50 +26,3 @@
 
 (defn on-click [ratom]
   (swap! ratom update-in [:count] inc))
-
-(defcard-doc
-  "
-## Rendering Reagent components
-Note: The following examples assume a namespace that looks like this:
-```clojure
-(ns xxx
-    (:require [devcards.core]
-              [reagent.core :as reagent])
-    (:require-macros [devcards.core :as dc
-                                    :refer [defcard defcard-rg]]))
-```
-")
-
-(defcard
-    "## Devcard Reagent basics
-First of all, you don't need to use any of Devcards helpers to use Reagent.
-You can use the `reagent.core/as-element` function to turn your
-reagent code into a ReactElement.
-The following works great:
-```clojure
-(defcard reagent-no-help
-  (reagent/as-element [:h1 \"Reagent example\"]))
-```
-You can see the example rendered below:
-")
-
-(defcard reagent-no-help
-
-  (reagent/as-element [:h1 "Reagent example"]))
-
-(defcard
-  "This will also work with Reagent components
-```clojure
-(defn reagent-component-example []
-  [:div \"hey there\"])
-(defcard reagent-no-help
-  (reagent/as-element [reagent-component-example]))
-```
-And you can see this rendered below:
-")
-
-(defn reagent-component-example []
-  [:div "hey there"])
-
-(defcard reagent-component
-  (reagent/as-element [reagent-component-example]))
