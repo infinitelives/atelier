61c8cb3edd0199df63969f2a6ff17f0c3559ab4b
Crispin Wellington <retrogradeorbit@gmail.com>
2016-05-15 22:24:48 +0800

    initialise a static app page with canvas and code together

--- src/atelier/core.cljs
+++ src/atelier/core.cljs
@@ -26,3 +26,79 @@
 
 (defn on-click [ratom]
   (swap! ratom update-in [:count] inc))
+
+(defonce editor-state (reagent/atom {:value "{
+  :foo
+  {
+    :size [8 8]
+    :pos [8 8]
+  }
+}"
+                                     :cursor {:line 1 :ch 0}}))
+
+(defonce canvas-state (reagent/atom {
+   :highlights
+   [
+    {:pos [9 12]
+     :size [1 10]}
+    ]
+
+   :scale 3
+
+   :offset [0 0]
+   }))
+
+(defn simple-component []
+  [:div
+
+   [:div {:style {:position "absolute"
+                  :display "block"
+                  :width "100%"
+                  :visibility "visible"
+                  :overflow "visible"
+
+                  :margin "0px"
+                  :left "0px"
+                  :right "auto"
+                  :top "0px"
+                  }}
+    (canvas/image-canvas canvas-state :width 1190 :height 900)]
+
+    [:div {:style {:position "absolute"
+                  :display "block"
+                  ;:height "739px"
+                  :width "10px"
+                  :visibility "visible"
+                  :overflow "visible"
+                  :border "1px solid black"
+                  :background-color "#aaa"
+
+                  :margin "0px"
+                  :left "1190px"
+                  :right "0px"
+                  :top "0px"
+                  :bottom "0px"
+                  }}]
+
+   [:div {:style {:position "absolute"
+                  :display "block"
+                  ;:height "739px"
+                  :width "*"
+                  :visibility "visible"
+                  :overflow "visible"
+
+                  :margin "0px"
+                  :left "1202px"
+                  :right "0px"
+                  :top "0px"
+                  ;:bottom "0px"
+                  }}
+    (code/editor editor-state :width 705 :height 900)]
+   ])
+
+(defn render-simple []
+  (reagent/render-component
+   [simple-component]
+   (.getElementById js/document "app")))
+
+(render-simple)
