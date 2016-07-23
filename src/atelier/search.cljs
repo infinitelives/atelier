dce302ddf90d1072f4ea9e4bcaa1b7a122634f4b
Crispin Wellington <retrogradeorbit@gmail.com>
2016-07-23 23:26:15 +0800

    rename char to sub

--- src/atelier/search.cljs
+++ src/atelier/search.cljs
@@ -5,37 +5,32 @@
 (defn forward-search-string
   "search a `string` from position `pos` forwards until `sub` is
   found. returns nil if the substring isn't found"
-  [string pos char]
-  (let [found (.indexOf (subs string pos) char)]
+  [string pos sub]
+  (let [found (.indexOf (subs string pos) sub)]
     (when (!-1 found)
       (+ pos found))))
 
 (defn reverse-search-string
   "search a `string` from position `pos` backwards until `sub` is
   found. returns nil if the substring isn't found"
-  [string pos char]
-  (let [found (.lastIndexOf (subs string 0 (+ pos (count char))) char)]
+  [string pos sub]
+  (let [found (.lastIndexOf (subs string 0 (+ pos (count sub))) sub)]
     (when (!-1 found)
       found)))
 
 (defn reverse-search [get-line start-char end-char line curs]
   (when-let [str (get-line line)]
-    (let [
-          substring (subs str 0 curs)
-          end-index (.indexOf substring end-char)
-          start-index (.indexOf substring start-char)
-          end-found? (!-1 end-index)
-          start-found? (!-1 start-index)
-          ]
-      ;; have we found an end-char? if so abort
+    (let [start (reverse-search-string str curs start-char)
+          end (reverse-search-string str curs end-char)]
       (cond
-        (or
-         (and end-found? start-found? (> end-index start-index))
-         (and end-found? (not start-found?)))
+        (and end start (> end start))
         nil
 
-        start-found?
-        [line start-index]
+        (and end (not start))
+        nil
+
+        start
+        [line start]
 
         (> line 0)
         (recur get-line
