621e9091b1a6b16eb2530cdab9d3c1bafddf0211
Crispin Wellington <retrogradeorbit@gmail.com>
2016-07-23 20:24:46 +0800

    failing tests

--- test/atelier/search_test.cljs
+++ test/atelier/search_test.cljs
@@ -28,6 +28,37 @@
   (is
    (= (read-string (s/from-to src-lines [1 7] [2 20]))
       {:pos [20 20] :size [10 10]})))
+
+(comment
+  (deftest nested-search
+    (is
+     (= (s/reverse-search src-lines "{" "}" 3 0)
+        [0 0]))
+    (is
+     (= (s/forward-search src-lines "{" "}" 1 0)
+        [8 0])))
+
+  (def src2 "{
+  :foo {
+  :bar {
+  :baz [1 2]
+  }
+  }
+}")
+
+  (def src2-lines (string/split src2 #"\n"))
+
+  (deftest expand-hashmap
+    (is
+     (= (s/expand-hashmap src2-lines {:start [3 10] :end [3 10]})
+        {:start [2 9] :end [4 4]}))
+    (is
+     (= (s/expand-hashmap src2-lines {:start [2 9] :end [4 4]})
+        {:start [1 7] :end [5 2]}))
+    (is
+     (= (s/expand-hashmap src2-lines {:start [1 7] :end [5 2]})
+        {:start [0 0] :end [6 0]}))))
+
 (deftest forward-search-string
   (is (= (s/forward-search-string "foo bar baz" 0 "bar") 4))
   (is (= (s/forward-search-string "foo bar baz" 4 "bar") 4))
