7e720b7711ef20d0534b3e62f67872a4c475cdaa
Crispin Wellington <retrogradeorbit@gmail.com>
2016-07-24 22:48:01 +0800

    html5 file api functions

---
+++ src/atelier/file.cljs
@@ -0,0 +1,34 @@
+(ns atelier.file
+  (:require [infinitelives.utils.console :refer [log]]
+            [infinitelives.pixi.resources :as resources]
+            [cljs.core.async :refer [<! chan put! alts! timeout]]
+            )
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  )
+
+;; HTML5 File API code
+
+(defn filelist->seq [filelist]
+  (for [n (range (.-length filelist))] (aget filelist n)))
+
+(defn read-file [file & [type]]
+  (let [c (chan)
+        reader (js/FileReader.)]
+    (set! (.-onload reader) #(put! c (.-target.result %)))
+    (case type
+      :binary-string (.readAsBinaryString reader file)
+      :text (.readAsText reader file)
+      :array-buffer (.readAsArrayBuffer reader file)
+      (:data-url nil) (.readAsDataURL reader file))
+    c))
+
+(defn handle-file-select [event]
+  (let [files (filelist->seq (.-target.files event))
+        file (first files)]
+    (go
+      (log "recieved:" (<! (read-file file :data-url))))))
+
+(defn file-selection []
+  [:input#files
+   {:type "file" :name "files" :multiple true
+    :on-change handle-file-select}])
